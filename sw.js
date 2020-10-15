34ef1545d3eb5dfec4891d1e0a1bd4583b1ce26c
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-10-14 21:33:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
 //service worker
+let files = [
+    "./index.html",
+    "./home.html",
+    "./login.html",
+    "./register.html",
+    "./style.css",
+    "./general.css",
+    "./src/api.js",
+    "./src/cookie.js",
+    "./src/custom.js",
+    "./src/home.js",
+    "./src/index.js",
+    "./src/login.js",
+    "./src/mobile.js",
+    "./images/background.jpg",
+    "./images/background2.jpg",
+    "./images/background3.jpg",
+    "./images/background4.jpg",
+    "./images/background5.jpg",
+    "./images/default.jpg",
+    "./images/fond.png",
+    "./images/home.png",
+    "./images/logo.png"
+];
+
+
