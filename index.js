6e69dfbe9c7146fd58addac28d3c3cf4e3a6eaa8
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-10-14 22:40:02 -0400

    Update and rename index.js to main.js

--- index.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator)
-{
-    navigator.serviceWorker.register("./sw.js", {scope:"./"}).then((register)=>{
-        
-    })
-}
---
+++ main.js
@@ -0,0 +1,19 @@
+
+if ('serviceWorker' in navigator)
+		{navigator.serviceWorker
+			.register('./sw.js', {
+				scope: './'
+			})
+			.then(reg => {
+				reg.onupdatefound = () => {
+					const installingWorker = reg.installing;
+					installingWorker.onstatechange = () => {
+						if (installingWorker.state == 'installed') {
+							if (navigator.serviceWorker.controller) resolve(true);
+							else resolve(false);
+						}
+					};
+				};
+			});
+
+        }
